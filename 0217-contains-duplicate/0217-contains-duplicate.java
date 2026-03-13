55f49d31af4573ed550511ac25a0341b9819b231
Niharika-coder-art <niha211206@gmail.com>
2026-03-13 21:00:00 +0530

    Time: 25 ms (13.37%), Space: 76.6 MB (97%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int size=nums.length;
+        for(int i=0;i<size-1;i++)
+        {
+                if(nums[i]==nums[i+1])
+                {
+                        return true;
+                }
+        }
+        return false;
+        
+    }
+}
